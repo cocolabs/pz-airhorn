2d5f3b5f5e47c60bf28a8824b65b82663c0a34c0
Dane <jordane.haroutunian@outlook.fr>
2021-02-21 00:38:02 +0100

    Refactor lua code

--- media/lua/client/AirHorn.lua
+++ media/lua/client/AirHorn.lua
@@ -1,35 +1,25 @@
 local function AirHorn()
 
-local player = getPlayer()
-local item = player:getPrimaryHandItem()
+  ---@type IsoGameCharacter
+  local player = getPlayer();
 
+  ---@type InventoryItem
+  local item = player:getPrimaryHandItem();
 
   if item and item:getType() == "AirHorn" and player:IsAiming() then
 
-    local condition = item:getCondition();
+    local condition = item:getCondition() - 1;
 
-    item:setCondition(condition - 1);
-
+    item:setCondition(condition);
 
     if condition ~= 0 then
-
-      getSoundManager():PlayWorldSound("AirHorn",player:getSquare(),0, 10, 10.0, false);
+      getSoundManager():PlayWorldSound("AirHorn", player:getSquare(), 0, 10, 10.0, false);
       addSound(player, player:getX(), player:getY(), player:getZ(), 250, 50);
-
-    elseif condition == 0 then
-
-      player:Say("Broken")
+    else
 
     end
-
-
-
   else
-
-    player:Say('NO');
-
+    
   end
-
 end
-
 Events.OnMouseUp.Add(AirHorn)
